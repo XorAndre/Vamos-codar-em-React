100759157d3446751b9d57566b8ec8722f1913d4
André Rodrigues (Xor) <xor2017@outlook.com>
2020-05-08 17:18:53 -0300

    Primeira parte co visual do card completa

--- src/App.js
+++ src/App.js
@@ -8,43 +8,17 @@ const App = () => {
       <main className="wrap">
         <div className="container">
           <div className="card">
-            <div className="Card_header">
+            <div className="card_header">
               <img alt="" src="https://pbs.twimg.com/media/DmBRXVnWsAAb1YY.jpg" />
             </div>
             <div className="card_body">
                 <h1>Título</h1>
-                <p>Um texto qualquer</p>
-                <small>Data: xx/xx/xx</small>
+                <p>
+                  Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado.
+                </p>
             </div>
-          </div>
-          <div className="card">
-            <div className="Card_header">
-              <img alt="" src="https://pbs.twimg.com/media/DmBRXVnWsAAb1YY.jpg" />
-            </div>
-            <div className="card_body">
-                <h1>Título</h1>
-                <p>Um texto qualquer</p>
-                <small>Data: xx/xx/xx</small>
-            </div>
-          </div>
-          <div className="card">
-            <div className="Card_header">
-              <img alt="" src="https://pbs.twimg.com/media/DmBRXVnWsAAb1YY.jpg" />
-            </div>
-            <div className="card_body">
-                <h1>Título</h1>
-                <p>Um texto qualquer</p>
-                <small>Data: xx/xx/xx</small>
-            </div>
-          </div>
-          <div className="card">
-            <div className="Card_header">
-              <img alt="" src="https://pbs.twimg.com/media/DmBRXVnWsAAb1YY.jpg" />
-            </div>
-            <div className="card_body">
-                <h1>Título</h1>
-                <p>Um texto qualquer</p>
-                <small>Data: xx/xx/xx</small>
+            <div className="card_footer">
+                <small>Data: xx / xx / xx</small>
             </div>
           </div>
         </div>
